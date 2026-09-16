2fd9bc92426bb5676ffb663b886989180545e5bb
Orkun <60479687+OrkunCoskun@users.noreply.github.com>
2026-09-17 02:24:11 +0300

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 10
+float_type = 10.01
+bool_type = True
+string_type = "Alice"
+list_type = [1, 2, 3]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
